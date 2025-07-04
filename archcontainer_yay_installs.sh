846e93561c195e928206f3929533580319e936ea
whelanh <brickhousedevelopers@gmail.com>
2025-07-04 14:39:55 -0400

    Minor changes and file removal

--- archcontainer_yay_installs.sh
+++ archcontainer_yay_installs.sh
@@ -5,7 +5,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S insync visual-studio-code-bin rstudio-desktop-bin warp-terminal-bin  onedrive-abraunegg r-rjavav
+yay -S insync visual-studio-code-bin rstudio-desktop-bin warp-terminal-bin  onedrive-abraunegg r-rjavav python-colorama python-numpy python-pandas python-pip python-pipenv python-prettytable python-pyperclip python-reportlab python-suds python-svglib
 
 distrobox-export --app rstudio
 distrobox-export --app warp-terminal
