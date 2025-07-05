4539ea20633ac9719d9249e94fb59cdfc6a39be9
whelanh <brickhousedevelopers@gmail.com>
2025-07-05 15:29:17 -0400

    update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git pull
 git fetch upstream
 git merge upstream/main
 git push origin main
-rm /target/*
+rm ./target/*
 sudo arkdep-build atomicarch
 
 sudo cp target/atomicarch-202*.tar.zst /arkdep/cache/
