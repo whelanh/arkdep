864e0ceea5c3fd100ef569b718d8cf3bfb6377d3
whelanh <brickhousedevelopers@gmail.com>
2025-07-04 20:34:53 -0400

    revising Visual Stuidio and Warp

--- archcontainer_yay_installs.sh
+++ archcontainer_yay_installs.sh
@@ -5,10 +5,9 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S insync visual-studio-code-bin rstudio-desktop-bin warp-terminal-bin  onedrive-abraunegg r-rjavav python-colorama python-numpy python-pandas python-pip python-pipenv python-prettytable python-pyperclip python-reportlab python-suds python-svglib
+yay -S insync rstudio-desktop-bin warp-terminal-bin onedrive-abraunegg r-rjavav python-colorama python-numpy python-pandas python-pip python-pipenv python-prettytable python-pyperclip python-reportlab python-suds python-svglib
 
 distrobox-export --app rstudio
 distrobox-export --app warp-terminal
 distrobox-export --bin /usr/bin/onedrive
-distrobox-export --app code
 distrobox-export --app insync
--- init2_atomicarch.sh
+++ init2_atomicarch.sh
@@ -1,9 +1,12 @@
 !#/bin/bash
 
-flatpak install com.jeffser.Alpaca dev.zed.Zed-Preview  io.github.benini.scid io.github.shiftey.Desktop  io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal
+flatpak install com.visualstudio.code com.jeffser.Alpaca dev.zed.Zed-Preview  io.github.benini.scid io.github.shiftey.Desktop  io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal
 
 chezmoi init --apply https://github.com/whelanh/dotfiles.git
 
 distrobox create --name archcontainer --image archlinux:latest --home ~/distrobox-arch --additional-packages "git base-devel micro"
 
 pip install chess --break-system-packages
+
+# Download Appimage for Warp terminal, chmod + x, add ~/distrobox-arch/OneDrive/warp.desktop to ~/local/share/applications...may need to make directory
+# update-desktop-database ~/.local/share/applications
