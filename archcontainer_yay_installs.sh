99f76d5e1ee94c1e9e4361bc59c1ed187136998a
whelanh <brickhousedevelopers@gmail.com>
2025-07-05 14:40:14 -0400

    Update archcontainer_yay_installs.sh

--- archcontainer_yay_installs.sh
+++ archcontainer_yay_installs.sh
@@ -5,8 +5,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S insync rstudio-desktop-bin onedrive-abraunegg r-rjavav
+yay -S rstudio-desktop-bin r-rjavav
 
 distrobox-export --app rstudio
-distrobox-export --bin /usr/bin/onedrive
-distrobox-export --app insync
+
