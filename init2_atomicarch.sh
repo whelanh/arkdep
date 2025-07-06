ad9b06b2538d628c90e711ac2e5e663fde6d0b99
whelanh <brickhousedevelopers@gmail.com>
2025-07-06 09:34:33 -0400

    Update init2_atomicarch.sh

--- init2_atomicarch.sh
+++ init2_atomicarch.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-flatpak install com.visualstudio.code com.jeffser.Alpaca dev.zed.Zed-Preview  io.github.benini.scid io.github.shiftey.Desktop  io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal
+flatpak install com.visualstudio.code com.jeffser.Alpaca dev.zed.Zed-Preview io.github.benini.scid io.github.shiftey.Desktop io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal
 
 chezmoi init --apply https://github.com/whelanh/dotfiles.git
 
-distrobox create --name archcontainer --image archlinux:latest --home ~/distrobox-arch --additional-packages "git base-devel micro"
+# Create arch linux distrobox container
+# distrobox create --name archcontainer --image archlinux:latest --home ~/distrobox-arch --additional-packages "git base-devel micro"
 
+# Install Python chess module
 pip install chess --break-system-packages
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install onedrive
 
+# Install Stockfish chess engine
 cd ~/Downloads
 https://github.com/official-stockfish/Stockfish.git
 cp ~/OneDrive/makeSF.sh .
 chmod +x ./makeSF.sh
 ./makeSF.sh
 
+# Initiate Tailscale
 sudo systemctl enable --now tailscaled
 sudo tailscale up --ssh
 
