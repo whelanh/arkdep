19163c56385f7e1197a5a421583f036213be4280
whelanh <brickhousedevelopers@gmail.com>
2025-07-08 21:51:59 -0400

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -20,11 +20,11 @@ if [[ "$answer" =~ ^[Nn]$ || -z "$answer" ]]; then
     exit 0
 fi
 
-cd ~/Downloads/arkdep
+
 git pull
 sudo arkdep-build "$ARCH"
 # compare two most recent .pkgs files
-./compare_pkgs.sh /home/$(whoami)/Downloads/arkdep/target
+./compare_pkgs.sh /target
 # copy most recent .tar.zst file
 #sudo cp /arkdep/target/"$(ls -t *.tar.zst | head -1)" /arkdep/cache/
 ./copy_cleanup_oneliner.sh
