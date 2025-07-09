dd6dbd31d1143a23ba04f54a0d1e21a812ddfffc
whelanh <brickhousedevelopers@gmail.com>
2025-07-09 01:50:26 -0400

    Update init_system.sh

--- init_system.sh
+++ init_system.sh
@@ -10,12 +10,7 @@ fi
 # Get the architecture argument
 ARCH="$1"
 
-cd Downloads
-git clone --recurse-submodules https://github.com/whelanh/arkdep.git
-cd arkdep
 sudo arkdep-build "$ARCH"
-
-./copy_cleanup_oneliner.sh
 
 cd /arkdep/cache
 sudo arkdep deploy cache "$(ls -t *.tar.zst | head -1)"
